f8a49df448b6a7eae6eb3a573e571800cf00d197
FabianCaspers <fabian.caspers1308@gmail.com>
2023-07-02 19:35:53 +0200

    style login&register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { SearchComponent } from './components/search/search.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
   { path: 'home', component: HomeComponent},
   { path:'search', component: SearchComponent},
   { path:'movie/:id', component: MovieDetailsComponent}
